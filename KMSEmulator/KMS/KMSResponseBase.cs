75c7bca6d1882fdfbaf60d7077eab0af24bd0771
Aaron Weiler <aaron@santacruzcomputersupport.com>
2016-09-24 22:15:38 -0700

    Added initial files provided on the MDL forums.
    https://forums.mydigitallife.info/threads/71213-Source-C-KMS-Server-from-Microsoft-Toolkit

---
+++ KMSEmulator/KMS/KMSResponseBase.cs
@@ -0,0 +1,35 @@
+﻿namespace KMSEmulator.KMS
+{
+    abstract class KMSResponseBase
+    {
+        public abstract uint BodyLength
+        {
+            get;
+        }
+
+        public byte[] Unknown
+        {
+            get
+            {
+                return new byte[] { 0x00, 0x00, 0x02, 0x00 };
+            }
+        }
+        public uint BodyLength2
+        {
+            get
+            {
+                return BodyLength;
+            }
+        }
+
+        public byte[] Padding
+        {
+            get
+            {
+                uint currentLength = BodyLength;
+                uint remainder = currentLength % 8;
+                return remainder == 0 ? new byte[0] : new byte[8 - remainder];
+            }
+        }
+    }
+}
